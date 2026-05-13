e652fdd9cfba95918a1e86979f6803b5f05d11ed
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-13 09:34:42 +0200

    add: implement free_list function for memory management of linked list

---
+++ src/list/free_list.c
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2025
+** bootstrap
+** File description:
+** free_list
+*/
+
+#include "../../include/corewar.h"
+
+void free_list(node_t **head)
+{
+    node_t *temp;
+    node_t *current = *head;
+
+    while (current != NULL) {
+        temp = current->next;
+        free(current);
+        current = temp;
+    }
+    *head = NULL;
+}
