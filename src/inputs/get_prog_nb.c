c1f9168c7d292ca1bb45b7365d39b7579c368fc5
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-13 03:59:10 +0200

    add: implement get_prog_nb function for program number validation

---
+++ src/inputs/get_prog_nb.c
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2026
+** corewar
+** File description:
+** get_prog_nb
+*/
+
+#include "../../include/corewar.h"
+
+void get_prog_nb(stock_main_t *main, char **argv, int pos)
+{
+    int prog_nb;
+
+    if (argv[pos] == NULL) {
+        write(2, "./corewar: -n: missing argument\n", 32);
+        exit(84);
+    }
+    prog_nb = atoi(argv[pos]);
+    if (prog_nb <= 0) {
+        write(2, "./corewar: -n: invalid argument\n", 32);
+        exit(84);
+    }
+    main->pending_prog_nb = prog_nb;
+}
