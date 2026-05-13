4f4be3d889cc0c9c9ad665c828739af4d4f1f36c
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-13 10:31:09 +0200

    add: define process_t structure for process management

--- include/core/core.h
+++ include/core/core.h
@@ -21,11 +21,19 @@ typedef struct champ_info_s {
     int pending_load_adress;
     int champion_count;
 }champ_info_t;
+typedef struct process_s {
+    int pc;
+    int reg[REG_NUMBER];
+    bool carry;
+    bool alive;
+    int last_live;
+}process_t;
 typedef struct stock_maint_s {
     unsigned char map[MEM_SIZE];
     unsigned int nb_cycle;
     unsigned int cycle_to_die;
     champ_info_t champ_info;
+    node_t *processes;
 } stock_main_t;
 typedef struct fcall_s {
     char *name;
