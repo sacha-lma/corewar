cbc3f97b8e64525a0efceab773670e6c631fe3c2
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-13 09:34:27 +0200

    add: declare merge sort functions for linked list operations

--- include/linked_lists/linked_lists.h
+++ include/linked_lists/linked_lists.h
@@ -17,4 +17,9 @@ typedef struct node_s {
     struct node_s *next;
 } node_t;
 void push_back(node_t **head, void *data);
+node_t *merge_sort_lists(node_t **head, int (*cmp_func)(void *, void *));
+node_t *merge_sort_merge_list(node_t *left, node_t *right,
+    int (*cmp_func)(void *, void *));
+void free_list(node_t **head);
+node_t *get_to_middle(node_t *head);
 #endif
