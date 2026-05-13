8cc03edd0f632accfbc8daf8e32765105c78122d
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-13 09:35:29 +0200

    refactor: remove unused champion variable and related print statements in setup function

--- src/init/setup.c
+++ src/init/setup.c
@@ -9,15 +9,8 @@
 
 int setup(stock_main_t *main)
 {
-    champion_t *champion = NULL;
-
     update_prog_nbr(main);
     main->champ_info.champions =
         merge_sort_lists(&main->champ_info.champions, prog_sorter);
-    for (node_t *current = main->champ_info.champions; current != NULL;
-        current = current->next) {
-        champion = current->data;
-        printf("champ prog = %d\n", champion->prog_nb);
-    }
     return 0;
 }
