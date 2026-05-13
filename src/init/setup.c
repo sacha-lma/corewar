89a0a9d0f8b27565e2c19c756c0240782ea65ce8
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-13 09:34:38 +0200

    add: integrate champion sorting and display in setup function

--- src/init/setup.c
+++ src/init/setup.c
@@ -9,6 +9,15 @@
 
 int setup(stock_main_t *main)
 {
+    champion_t *champion = NULL;
+
     update_prog_nbr(main);
+    main->champ_info.champions =
+        merge_sort_lists(&main->champ_info.champions, prog_sorter);
+    for (node_t *current = main->champ_info.champions; current != NULL;
+        current = current->next) {
+        champion = current->data;
+        printf("champ prog = %d\n", champion->prog_nb);
+    }
     return 0;
 }
