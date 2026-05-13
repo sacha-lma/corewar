9bf445affa51f50b03b20a6eea1f57334f486d8f
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-13 03:58:58 +0200

    add: create display.h header file with print_help function declaration

---
+++ include/display/display.h
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2026
+** corewar
+** File description:
+** display
+*/
+
+#include "../corewar.h"
+
+#ifndef DISPLAY_H
+    #define DISPLAY_H
+void print_help(stock_main_t *main, char **argv, int pos);
+#endif
