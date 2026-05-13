030cd9d507aa5ce6e3fff9208887f2716dcba380
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-13 09:34:49 +0200

    add: implement merge_sort_merge_list function for merging sorted linked lists

---
+++ src/list/merge_sort_merge_list.c
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2025
+** organized
+** File description:
+** merge_sort_merge_list
+*/
+
+#include "../../include/corewar.h"
+
+node_t *merge_sort_merge_list(node_t *left, node_t *right,
+    int (*cmp_func)(void *, void *))
+{
+    node_t *result = NULL;
+
+    if (left == NULL)
+        return right;
+    if (right == NULL)
+        return left;
+    if (cmp_func(left, right) > 0) {
+        result = left;
+        result->next = merge_sort_merge_list(left->next, right, cmp_func);
+    } else {
+        result = right;
+        result->next = merge_sort_merge_list(left, right->next, cmp_func);
+    }
+    return result;
+}
