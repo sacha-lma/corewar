bf13335211b5c5e8e8fe290ff8d09b8b5c80dfc3
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-13 03:59:19 +0200

    add: create init.h header file with function declarations for initialization

---
+++ include/init/init.h
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2026
+** corewar
+** File description:
+** init
+*/
+
+#ifndef INIT_H
+    #define INIT_H
+void init(stock_main_t *main);
+int setup(stock_main_t *main);
+#endif
