00196458b998c1615211e899f0891cacf0a7b30a
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-13 09:08:49 +0200

    fix: update initialization to use champ_info structure for champions and related fields

--- src/init/init.c
+++ src/init/init.c
@@ -11,9 +11,9 @@ void init(stock_main_t *main)
 {
     memset(main->map, 0, MEM_SIZE);
     main->nb_cycle = 0;
-    main->champions = NULL;
-    main->pending_prog_nb = -1;
-    main->pending_load_adress = -1;
-    main->champion_count = 0;
+    main->champ_info.champions = NULL;
+    main->champ_info.pending_prog_nb = -1;
+    main->champ_info.pending_load_adress = -1;
+    main->champ_info.champion_count = 0;
     main->cycle_to_die = CYCLE_TO_DIE;
 }
