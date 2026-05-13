99573d81725cac665e4927ad66ae46678349dde9
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-13 09:34:45 +0200

    add: implement get_to_middle function to find the middle of a linked list

---
+++ src/list/get_to_middle.c
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2025
+** organized
+** File description:
+** get_to_middle
+*/
+
+#include "../../include/corewar.h"
+
+node_t *get_to_middle(node_t *head)
+{
+    node_t *slow = head;
+    node_t *fast = head->next;
+
+    while (fast != NULL) {
+        fast = fast->next;
+        if (fast != NULL) {
+            slow = slow->next;
+            fast = fast->next;
+        }
+    }
+    return slow;
+}
