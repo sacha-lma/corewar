f1927d44bf9cf1ba67c256cf3a43698e4f64c21e
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-13 03:59:02 +0200

    add: implement get_cycles function to handle cycle input validation

---
+++ src/inputs/get_cycles.c
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2026
+** corewar
+** File description:
+** get_cycles
+*/
+
+#include "../../include/corewar.h"
+
+void get_cycles(stock_main_t *main, char **argv, int pos)
+{
+    int cycles;
+
+    if (argv[pos] == NULL) {
+        write(2, "./corewar: -dump: missing argument\n", 35);
+        exit(84);
+    }
+    cycles = atoi(argv[pos]);
+    if (cycles <= 0) {
+        write(2, "./corewar: -dump: invalid argument\n", 35);
+        exit(84);
+    }
+    main->nb_cycle = (unsigned int)cycles;
+}
