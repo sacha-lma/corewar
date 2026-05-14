5aa4b06ba5627902b7e5cbaee430e68b0bae5421
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-15 01:02:34 +0200

    refactor: change function declarations in inputs.h to return int

--- include/inputs/inputs.h
+++ include/inputs/inputs.h
@@ -5,12 +5,11 @@
 ** inputs
 */
 
-#include "../corewar.h"
-
 #ifndef INPUTS_H
     #define INPUTS_H
+    #include "core/core.h"
 int manage_args(int argc, char **argv, stock_main_t *main_struct);
-void get_prog_nb(stock_main_t *main_struct, char **argv, int pos);
-void get_load_adress(stock_main_t *main_struct, char **argv, int pos);
-void get_cycles(stock_main_t *main_struct, char **argv, int pos);
+int get_prog_nb(stock_main_t *main_struct, char **argv, int pos);
+int get_load_adress(stock_main_t *main_struct, char **argv, int pos);
+int get_cycles(stock_main_t *main_struct, char **argv, int pos);
 #endif
