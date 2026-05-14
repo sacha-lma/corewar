c665a515716ec7acaebb50d90f0c0e27adba3201
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-14 19:13:39 +0200

    add: create utils.h header file for utility functions and definitions

---
+++ include/utils/utils.h
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2026
+** corewar
+** File description:
+** utils
+*/
+
+#ifndef UTILS_H
+    #define UTILS_H
+    #include "len/len.h"
+    #include "linked_lists/linked_lists.h"
+#endif
