a4ea167b1779837d7d14f59cc4578e4e429ae066
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-14 19:31:30 +0200

    refactor: rename variable main to main_struct for clarity in setup.c

--- src/init/setup.c
+++ src/init/setup.c
@@ -7,11 +7,12 @@
 
 #include "../../include/corewar.h"
 
-int setup(stock_main_t *main)
+int setup(stock_main_t *main_struct)
 {
-    update_prog_nbr(main);
-    main->champ_info.champions =
-        merge_sort_lists(&main->champ_info.champions, prog_sorter);
-    champions_loader(main);
+    update_prog_nbr(main_struct);
+    main_struct->champ_info.champions =
+        merge_sort_lists(&main_struct->champ_info.champions, prog_sorter);
+    if (champions_loader(main_struct) == 84)
+        return 84;
     return 0;
 }
