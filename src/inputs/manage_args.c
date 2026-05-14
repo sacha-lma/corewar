5b175373d0d566fb9fe01a5f771df8c069572f96
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-14 19:32:06 +0200

    refactor: rename parameter main to main_struct for clarity in manage_args and related functions

--- src/inputs/manage_args.c
+++ src/inputs/manage_args.c
@@ -16,7 +16,7 @@ const fcall_t fcalls[] = {
     {NULL, NULL}
 };
 
-static int add_champion(int *i, char **argv, stock_main_t *main)
+static int add_champion(int *i, char **argv, stock_main_t *main_struct)
 {
     champion_t *champ;
 
@@ -26,32 +26,37 @@ static int add_champion(int *i, char **argv, stock_main_t *main)
     if (champ == NULL)
         return 84;
     champ->path = argv[*i];
-    main->champ_info.champion_count++;
-    champ->prog_nb = main->champ_info.pending_prog_nb == -1
-        ? -1 : (unsigned int)main->champ_info.pending_prog_nb;
-    champ->load_adress = main->champ_info.pending_load_adress;
-    main->champ_info.pending_prog_nb = -1;
-    main->champ_info.pending_load_adress = -1;
-    push_back(&main->champ_info.champions, champ);
+    main_struct->champ_info.champion_count++;
+    champ->prog_nb = main_struct->champ_info.pending_prog_nb == -1
+        ? -1 : (unsigned int)main_struct->champ_info.pending_prog_nb;
+    champ->load_adress = main_struct->champ_info.pending_load_adress;
+    main_struct->champ_info.pending_prog_nb = -1;
+    main_struct->champ_info.pending_load_adress = -1;
+    push_back(&main_struct->champ_info.champions, champ);
     return 0;
 }
 
-static int is_flag(int *i, char **argv, stock_main_t *main)
+static int call_func(int *i, int j, char **argv, stock_main_t *main_struct)
 {
-    for (int j = 0; fcalls[j].name != NULL; j++) {
-        if (strcmp(argv[*i], fcalls[j].name) == 0) {
-            fcalls[j].func(main, argv, *i + 1);
-            (*i)++;
-            return 0;
-        }
-    }
-    return add_champion(i, argv, main);
+    if (*i + 1 >= my_array_len(argv))
+        return 84;
+    fcalls[j].func(main_struct, argv, *i + 1);
+    (*i)++;
+    return 0;
 }
 
-int manage_args(const int argc, char **argv, stock_main_t *main)
+static int is_flag(int *i, char **argv, stock_main_t *main_struct)
+{
+    for (int j = 0; fcalls[j].name != NULL; j++)
+        if (strcmp(argv[*i], fcalls[j].name) == 0)
+            return call_func(i, j, argv, main_struct);
+    return add_champion(i, argv, main_struct);
+}
+
+int manage_args(const int argc, char **argv, stock_main_t *main_struct)
 {
     for (int i = 1; i < argc; i++)
-        if (is_flag(&i, argv, main) == 84)
+        if (is_flag(&i, argv, main_struct) == 84)
             return 84;
     return 0;
 }
