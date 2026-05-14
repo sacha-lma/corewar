18439afa4299b6ba738b784d1a794926c0cea9aa
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-14 19:32:16 +0200

    add: create loop.h header file with loop function declaration

---
+++ include/core/loop/loop.h
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2026
+** corewar
+** File description:
+** loop
+*/
+
+#include "../../corewar.h"
+
+#ifndef LOOP_H_
+    #define LOOP_H_
+void loop(stock_main_t *main_struct);
+#endif
