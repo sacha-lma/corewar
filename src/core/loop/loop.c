e40b518b69968eff9d229640f01cba37d1090e78
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-14 23:41:04 +0200

    refactor: update loop function to iterate over processes and handle opcodes

--- src/core/loop/loop.c
+++ src/core/loop/loop.c
@@ -8,14 +8,22 @@
 #include "../../../include/corewar.h"
 
 const process_call_t process_calls[] = {
-    {NULL, NULL}
+    {NULL}
 };
 
 void loop(stock_main_t *main_struct)
 {
-    for (node_t *temp = main_struct->champ_info.champions;
-        main_struct->champ_info.champion_count > 1
-        && main_struct->nb_cycle != 0;
+    process_t *current_process = NULL;
+    unsigned char opcode = 0;
+
+    for (node_t *temp = main_struct->processes;
+        temp != NULL;
         temp = temp->next) {
-    }
+        current_process = temp->data;
+        opcode = main_struct->map[current_process->pc % MEM_SIZE];
+        if (opcode >= 1 && opcode <= 16)
+            process_calls[opcode - 1].func(main_struct, current_process);
+        else
+            current_process->pc = (current_process->pc + 1) % MEM_SIZE;
+        }
 }
