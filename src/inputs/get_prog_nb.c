49db2d76c5b3c6e6403db1b894360cc809be5cce
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-15 01:02:20 +0200

    refactor: change get_prog_nb to return an int and use strtol for argument parsing

--- src/inputs/get_prog_nb.c
+++ src/inputs/get_prog_nb.c
@@ -7,18 +7,16 @@
 
 #include "../../include/corewar.h"
 
-void get_prog_nb(stock_main_t *main_struct, char **argv, int pos)
+int get_prog_nb(stock_main_t *main_struct, char **argv, int pos)
 {
-    int prog_nb;
+    char *endptr;
+    long prog_nb;
 
-    if (argv[pos] == NULL) {
-        write(2, "./corewar: -n: missing argument\n", 32);
-        exit(84);
-    }
-    prog_nb = atoi(argv[pos]);
-    if (prog_nb <= 0) {
+    prog_nb = strtol(argv[pos], &endptr, 10);
+    if (*endptr != '\0' || prog_nb <= 0) {
         write(2, "./corewar: -n: invalid argument\n", 32);
-        exit(84);
+        return 84;
     }
-    main_struct->champ_info.pending_prog_nb = prog_nb;
+    main_struct->champ_info.pending_prog_nb = (int)prog_nb;
+    return 0;
 }
