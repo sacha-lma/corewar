bf9ab3485d69800722fa15abc1f9d99c8c99a090
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-14 19:31:52 +0200

    add: implement my_array_len function to calculate the length of an array

---
+++ src/utils/len/my_array_len.c
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2026
+** corewar
+** File description:
+** my_array_len
+*/
+
+#include "../../../include/corewar.h"
+
+int my_array_len(char **array)
+{
+    int len = 0;
+
+    if (array == NULL)
+        return 0;
+    for (; array[len] != NULL; len++);
+    return len;
+}
