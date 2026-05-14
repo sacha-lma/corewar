86405e7c4cc2e5dd57f405bfa7f5cee1d03849f6
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-15 01:02:52 +0200

    refactor: enhance op_live function to track champion status and update process state

--- src/core/opperations/op_live.c
+++ src/core/opperations/op_live.c
@@ -9,4 +9,19 @@
 
 void op_live(stock_main_t *main_struct, process_t *process)
 {
+    int champion_nb = read_int_from_map(main_struct->map, process->pc + 1);
+    champion_t *champ = NULL;
+
+    for (node_t *temp = main_struct->champ_info.champions; temp != NULL;
+        temp = temp->next) {
+        champ = temp->data;
+        if (champ->prog_nb == champion_nb) {
+            dprintf(1, "The player %d(%s) is alive.\n",
+                champ->prog_nb, champ->header->prog_name);
+            main_struct->nb_live++;
+            process->last_live = main_struct->nb_cycle;
+            break;
+        }
+    }
+    process->pc = (process->pc + 5) % MEM_SIZE;
 }
