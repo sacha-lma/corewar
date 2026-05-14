4f86ae20b0418b8cf248753667c70d54f9690e71
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-15 01:02:15 +0200

    refactor: change get_load_adress to return an int and use strtol for argument parsing

--- src/inputs/get_load_adress.c
+++ src/inputs/get_load_adress.c
@@ -7,11 +7,16 @@
 
 #include "../../include/corewar.h"
 
-void get_load_adress(stock_main_t *main_struct, char **argv, int pos)
+int get_load_adress(stock_main_t *main_struct, char **argv, int pos)
 {
-    if (argv[pos] == NULL) {
-        write(2, "./corewar: -a: missing argument\n", 32);
-        exit(84);
+    char *endptr;
+    long addr;
+
+    addr = strtol(argv[pos], &endptr, 10);
+    if (*endptr != '\0') {
+        write(2, "./corewar: -a: invalid argument\n", 32);
+        return 84;
     }
-    main_struct->champ_info.pending_load_adress = atoi(argv[pos]);
+    main_struct->champ_info.pending_load_adress = (int)addr;
+    return 0;
 }
