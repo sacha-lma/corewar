a57ed32b2f169f020696cb174103038209db19e5
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-14 19:32:31 +0200

    refactor: rename parameter main to main_struct for clarity in inputs.h

--- include/inputs/inputs.h
+++ include/inputs/inputs.h
@@ -9,8 +9,8 @@
 
 #ifndef INPUTS_H
     #define INPUTS_H
-int manage_args(int argc, char **argv, stock_main_t *main);
-void get_prog_nb(stock_main_t *main, char **argv, int pos);
-void get_load_adress(stock_main_t *main, char **argv, int pos);
-void get_cycles(stock_main_t *main, char **argv, int pos);
+int manage_args(int argc, char **argv, stock_main_t *main_struct);
+void get_prog_nb(stock_main_t *main_struct, char **argv, int pos);
+void get_load_adress(stock_main_t *main_struct, char **argv, int pos);
+void get_cycles(stock_main_t *main_struct, char **argv, int pos);
 #endif
