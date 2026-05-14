74613a102e2ddefac065a24ce796e7db717447a5
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-15 01:01:48 +0200

    refactor: add closup function to clean up champions and processes

---
+++ src/init/closup.c
@@ -0,0 +1,46 @@
+/*
+** EPITECH PROJECT, 2026
+** corewar
+** File description:
+** closup
+*/
+
+#include "../../include/corewar.h"
+
+static void free_champions(node_t **head)
+{
+    champion_t *champ;
+    node_t *current = *head;
+    node_t *temp;
+
+    while (current != NULL) {
+        champ = current->data;
+        free(champ->header);
+        free(champ);
+        temp = current->next;
+        free(current);
+        current = temp;
+    }
+    *head = NULL;
+}
+
+static void free_processes(node_t **head)
+{
+    node_t *current = *head;
+    node_t *temp;
+
+    while (current != NULL) {
+        free(current->data);
+        temp = current->next;
+        free(current);
+        current = temp;
+    }
+    *head = NULL;
+}
+
+void closup(stock_main_t *main_struct)
+{
+    free_champions(&main_struct->champ_info.champions);
+    free_processes(&main_struct->processes);
+    free(main_struct);
+}
