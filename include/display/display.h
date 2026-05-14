07176d84690ae9cf2a6576fb98327347b7919711
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-15 01:02:07 +0200

    refactor: update display.h to include core.h and change print_help function signature

--- include/display/display.h
+++ include/display/display.h
@@ -5,9 +5,8 @@
 ** display
 */
 
-#include "../corewar.h"
-
 #ifndef DISPLAY_H
     #define DISPLAY_H
-void print_help(stock_main_t *main_struct, char **argv, int pos);
+    #include "core/core.h"
+int print_help(stock_main_t *main_struct, char **argv, int pos);
 #endif
