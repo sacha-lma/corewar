9489e5cb5f81c82421a4a7f0a265de2bc7b3a73a
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-14 19:31:35 +0200

    refactor: update set_process function to return status and rename parameter for clarity

--- src/init/metadatta/set_process.c
+++ src/init/metadatta/set_process.c
@@ -7,15 +7,18 @@
 
 #include "../../../include/corewar.h"
 
-void set_process(stock_main_t *main, int load_add, champion_t *champion)
+int set_process(stock_main_t *main_struct, int load_add, champion_t *champion)
 {
     process_t *process = malloc(sizeof(process_t));
 
+    if (!process)
+        return 84;
     process->pc = load_add;
-    memset(process->reg, 0, REG_SIZE);
+    memset(process->reg, 0, REG_SIZE * REG_NUMBER);
     process->reg[0] = -champion->prog_nb;
     process->carry = false;
     process->alive = true;
     process->last_live = 0;
-    push_back(&main->processes, process);
+    push_back(&main_struct->processes, process);
+    return 0;
 }
