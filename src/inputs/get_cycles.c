04757a0fe80ef8f8a7b86aeb7df7a460d1315f32
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-15 01:02:11 +0200

    refactor: change get_cycles function to return an int and use strtol for argument parsing

--- src/inputs/get_cycles.c
+++ src/inputs/get_cycles.c
@@ -7,18 +7,16 @@
 
 #include "../../include/corewar.h"
 
-void get_cycles(stock_main_t *main_struct, char **argv, int pos)
+int get_cycles(stock_main_t *main_struct, char **argv, int pos)
 {
-    int cycles;
+    char *endptr;
+    long cycles;
 
-    if (argv[pos] == NULL) {
-        write(2, "./corewar: -dump: missing argument\n", 35);
-        exit(84);
-    }
-    cycles = atoi(argv[pos]);
-    if (cycles <= 0) {
+    cycles = strtol(argv[pos], &endptr, 10);
+    if (*endptr != '\0' || cycles <= 0) {
         write(2, "./corewar: -dump: invalid argument\n", 35);
-        exit(84);
+        return 84;
     }
-    main_struct->nb_cycle = (unsigned int)cycles;
+    main_struct->nb_cycle = (int)cycles;
+    return 0;
 }
