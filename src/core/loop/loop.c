24338db229d6c5845c7fff128da2b7cd7dfe8e33
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-14 22:54:25 +0200

    refactor: remove unnecessary debug print statements in loop.c for cleaner output

--- src/core/loop/loop.c
+++ src/core/loop/loop.c
@@ -17,7 +17,5 @@ void loop(stock_main_t *main_struct)
         main_struct->champ_info.champion_count > 1
         && main_struct->nb_cycle != 0;
         temp = temp->next) {
-        printf("champ = %s\n", ((champion_t *)temp->data)->header->prog_name);
-        main_struct->champ_info.champion_count--;
     }
 }
