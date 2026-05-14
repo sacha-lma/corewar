b4b7fc93597ee7fac6b5ce03ce2034760470a530
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-14 19:32:20 +0200

    add: implement loop function to process champion information in corewar

---
+++ src/core/loop/loop.c
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2026
+** corewar
+** File description:
+** loop
+*/
+
+#include "../../../include/corewar.h"
+
+const fcall_t process_calls[] = {
+    {NULL, NULL}
+};
+
+void loop(stock_main_t *main_struct)
+{
+    for (node_t *temp = main_struct->champ_info.champions;
+        main_struct->champ_info.champion_count > 1
+        && main_struct->nb_cycle != 0;
+        temp = temp->next) {
+        printf("champ = %s\n", ((champion_t *)temp->data)->header->prog_name);
+        main_struct->champ_info.champion_count--;
+    }
+}
