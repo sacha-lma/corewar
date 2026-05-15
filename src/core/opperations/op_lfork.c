70012dc37b8201a91dc0adf84d2bf083115e323a
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-15 17:42:44 +0200

    feat: implement op_lfork function to create a new process with adjusted program counter

--- src/core/opperations/op_lfork.c
+++ src/core/opperations/op_lfork.c
@@ -9,4 +9,13 @@
 
 void op_lfork(stock_main_t *main_struct, process_t *process)
 {
+    short arg = read_short_from_map(main_struct->map, process->pc + 1);
+    process_t *new_proc = malloc(sizeof(process_t));
+
+    if (!new_proc)
+        return;
+    *new_proc = *process;
+    new_proc->pc = (process->pc + arg) % MEM_SIZE;
+    push_back(&main_struct->processes, new_proc);
+    process->pc = (process->pc + OP_LFORK_SIZE) % MEM_SIZE;
 }
