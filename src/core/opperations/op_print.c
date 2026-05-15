68b4f856c171ffefd266950e11a1a4264fb4508b
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-15 18:01:42 +0200

    feat: replace dprintf with write for output in op_print function

--- src/core/opperations/op_print.c
+++ src/core/opperations/op_print.c
@@ -10,8 +10,11 @@
 void op_print(stock_main_t *main_struct, process_t *process)
 {
     int reg = main_struct->map[(process->pc + 1) % MEM_SIZE];
+    char output;
 
-    if (reg >= 1 && reg <= REG_NUMBER)
-        dprintf(1, "%c", process->reg[reg - 1] % 256);
+    if (reg >= 1 && reg <= REG_NUMBER) {
+        output = (char)(process->reg[reg - 1] % 256);
+        write(1, &output, 1);
+    }
     process->pc = (process->pc + 2) % MEM_SIZE;
 }
