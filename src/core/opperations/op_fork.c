444e0dc25f08cf08c1ece6df0da4abff5e119113
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-15 17:42:36 +0200

    feat: optimize op_fork function by simplifying argument handling and process creation

--- src/core/opperations/op_fork.c
+++ src/core/opperations/op_fork.c
@@ -9,22 +9,13 @@
 
 void op_fork(stock_main_t *main_struct, process_t *process)
 {
-    short offset = (main_struct->map[(process->pc + 1) % MEM_SIZE] << 8) |
-        (main_struct->map[(process->pc + 2) % MEM_SIZE]);
-    int new_pc;
-    process_t *child = malloc(sizeof(process_t));
+    short arg = read_short_from_map(main_struct->map, process->pc + 1);
+    process_t *new_proc = malloc(sizeof(process_t));
 
-    if (child == NULL)
+    if (!new_proc)
         return;
-    new_pc = (process->pc + (offset % IDX_MOD)) % MEM_SIZE;
-    if (new_pc < 0)
-        new_pc += MEM_SIZE;
-    child->pc = new_pc;
-    child->carry = process->carry;
-    child->last_live = process->last_live;
-    for (int i = 0; i < REG_NUMBER; i++)
-        child->reg[i] = process->reg[i];
-    push_back(&main_struct->processes, child);
-    process->pc = (process->pc + 3) % MEM_SIZE;
+    *new_proc = *process;
+    new_proc->pc = (process->pc + arg % IDX_MOD) % MEM_SIZE;
+    push_back(&main_struct->processes, new_proc);
+    process->pc = (process->pc + OP_FORK_SIZE) % MEM_SIZE;
 }
-
