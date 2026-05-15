be7c56f66d2826e809405dcd3ed1979c23557275
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-15 17:42:40 +0200

    feat: implement op_ld function to handle argument reading and process state updates

--- src/core/opperations/op_ld.c
+++ src/core/opperations/op_ld.c
@@ -9,4 +9,17 @@
 
 void op_ld(stock_main_t *main_struct, process_t *process)
 {
+    unsigned char coding = main_struct->map[(process->pc + 1) % MEM_SIZE];
+    int type1 = coding >> 6 & 0x03;
+    int arg1 = read_arg(main_struct->map, process->pc + 2, type1, 0);
+    int reg = main_struct->map[(process->pc + 2 +
+            get_arg_size(type1, 0)) % MEM_SIZE];
+
+    if (type1 == 0x03)
+        arg1 = read_int_from_map(main_struct->map,
+            (process->pc + arg1 % IDX_MOD) % MEM_SIZE);
+    if (reg >= 1 && reg <= REG_NUMBER)
+        process->reg[reg - 1] = arg1;
+    process->carry = (arg1 == 0) ? 1 : 0;
+    process->pc = (process->pc + 3 + get_arg_size(type1, 0)) % MEM_SIZE;
 }
