f7f4935069aeeff6b19b26facd0b0280e566dec7
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-15 13:50:04 +0200

    feat: add debug prints to op_live for champion number tracking

--- src/core/opperations/op_live.c
+++ src/core/opperations/op_live.c
@@ -12,9 +12,11 @@ void op_live(stock_main_t *main_struct, process_t *process)
     int champion_nb = read_int_from_map(main_struct->map, process->pc + 1);
     champion_t *champ = NULL;
 
+    printf("DEBUG live: champion_nb = %d\n", champion_nb);
     for (node_t *temp = main_struct->champ_info.champions; temp != NULL;
         temp = temp->next) {
         champ = temp->data;
+        printf("DEBUG live: champ->prog_nb = %d\n", champ->prog_nb);
         if (champ->prog_nb == champion_nb) {
             dprintf(1, "The player %d(%s) is alive.\n",
                 champ->prog_nb, champ->header->prog_name);
