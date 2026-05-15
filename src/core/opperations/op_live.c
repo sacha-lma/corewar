a2cd936ab7895d30665a79393c0e602f3e2c10e2
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-15 15:52:45 +0200

    feat: enhance live champion reporting with trimmed name length

--- src/core/opperations/op_live.c
+++ src/core/opperations/op_live.c
@@ -16,8 +16,10 @@ void op_live(stock_main_t *main_struct, process_t *process)
         temp = temp->next) {
         champ = temp->data;
         if (champ->prog_nb == -champion_nb) {
-            dprintf(1, "The player %d(%s) is alive.\n",
-                champ->prog_nb, champ->header->prog_name);
+            dprintf(1, "The player %d(%.*s )is alive.\n",
+                champ->prog_nb,
+                get_name_len(champ->header->prog_name),
+                champ->header->prog_name);
             main_struct->nb_live++;
             process->last_live = main_struct->nb_cycle;
             main_struct->last_live_champion = champ->prog_nb;
