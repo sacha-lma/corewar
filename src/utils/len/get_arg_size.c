086b49a5301f8a8e75c1c60504216d1b61032235
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-15 11:54:34 +0200

    feat: add get_arg_size function to determine argument size based on type and index status

---
+++ src/utils/len/get_arg_size.c
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2026
+** corewar
+** File description:
+** get_arg_size
+*/
+
+#include "../../../include/corewar.h"
+
+int get_arg_size(int type, int is_index)
+{
+    if (type == 0x01)
+        return 1;
+    if (type == 0x02)
+        return is_index ? 2 : 4;
+    if (type == 0x03)
+        return 2;
+    return 0;
+}
