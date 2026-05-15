7b53e0b0178014c7144ab989e80d8921d2739cc6
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-15 17:43:04 +0200

    feat: enhance op_print to output register value based on program counter

--- src/core/opperations/op_print.c
+++ src/core/opperations/op_print.c
@@ -9,4 +9,9 @@
 
 void op_print(stock_main_t *main_struct, process_t *process)
 {
+    int reg = main_struct->map[(process->pc + 1) % MEM_SIZE];
+
+    if (reg >= 1 && reg <= REG_NUMBER)
+        dprintf(1, "%c", process->reg[reg - 1] % 256);
+    process->pc = (process->pc + 2) % MEM_SIZE;
 }
