59317d476295f6813b65c218ab4f0d1b389c0f9e
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-15 11:54:48 +0200

    feat: add read_short_from_map function to read short values from memory map

---
+++ src/utils/extraction/read_short_from_map.c
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2026
+** corewar
+** File description:
+** read_short_from_map
+*/
+
+#include "../../../include/corewar.h"
+
+short read_short_from_map(unsigned char *map, int pc)
+{
+    short val = 0;
+
+    val |= map[(pc + 0) % MEM_SIZE] << 8;
+    val |= map[(pc + 1) % MEM_SIZE];
+    return val;
+}
