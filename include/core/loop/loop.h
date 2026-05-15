b6332a7425e99325fb70b5b247757fa8a4a7e6d9
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-15 11:09:46 +0200

    refactor: change loop function return type and add new utility functions

--- include/core/loop/loop.h
+++ include/core/loop/loop.h
@@ -8,5 +8,7 @@
 #ifndef LOOP_H_
     #define LOOP_H_
     #include "core/core.h"
-void loop(stock_main_t *main_struct);
+int loop(stock_main_t *main_struct);
+void call_op(stock_main_t *main_struct);
+void check_deaths(stock_main_t *main_struct);
 #endif
