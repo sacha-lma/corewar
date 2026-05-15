13f579791fbd9beca81939cdd0a60464a130c651
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-15 17:43:19 +0200

    feat: implement op_zjmp function for zero jump operation based on carry flag

--- src/core/opperations/op_zjmp.c
+++ src/core/opperations/op_zjmp.c
@@ -9,4 +9,10 @@
 
 void op_zjmp(stock_main_t *main_struct, process_t *process)
 {
+    short arg = read_short_from_map(main_struct->map, process->pc + 1);
+
+    if (process->carry == 1)
+        process->pc = (process->pc + arg % IDX_MOD) % MEM_SIZE;
+    else
+        process->pc = (process->pc + OP_ZJMP_SIZE) % MEM_SIZE;
 }
