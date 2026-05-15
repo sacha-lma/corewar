9ff1a1ff7584d7ace756024b6ebf8b6eb3416a2f
Eris <maya1.dental@epitech.eu>
2026-05-15 15:54:16 +0200

    feat: add the operator "fork" in file op_fork.c

--- src/core/opperations/op_fork.c
+++ src/core/opperations/op_fork.c
@@ -9,4 +9,21 @@
 
 void op_fork(stock_main_t *main_struct, process_t *process)
 {
+    short offset = (main_struct->map[(process->pc + 1) % MEM_SIZE] << 8) |
+        (main_struct->map[(process->pc + 2) % MEM_SIZE]);
+    int new_pc;
+    process_t *child = malloc(sizeof(process_t));
+
+    if (child == NULL)
+        return;
+    new_pc = (process->pc + (offset % IDX_MOD)) % MEM_SIZE;
+    if (new_pc < 0)
+        new_pc += MEM_SIZE;
+    child->pc = new_pc;
+    child->carry = process->carry;
+    child->last_live = process->last_live;
+    for (int i = 0; i < REG_NUMBER; i++)
+        child->reg[i] = process->reg[i];
+    push_back(&main_struct->processes, child);
+    process->pc = (process->pc + 3) % MEM_SIZE;
 }
