763cd81f17b8e164dc9d8762e5c4d52f36d4a76d
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-15 11:53:57 +0200

    feat: add exraction.h for reading data from map

---
+++ include/utils/extraction/exraction.h
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2026
+** corewar
+** File description:
+** math
+*/
+
+#ifndef EXRACTION_H
+    #define EXRACTION_H
+int read_int_from_map(unsigned char *map, int pc);
+int read_arg(unsigned char *map, int pc, int type, int is_index);
+short read_short_from_map(unsigned char *map, int pc);
+#endif
