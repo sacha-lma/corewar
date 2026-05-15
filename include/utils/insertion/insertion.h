b95889f7518a31a3535edacf55336671d40025eb
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-15 11:54:39 +0200

    feat: add insertion.h header for integer writing functionality

---
+++ include/utils/insertion/insertion.h
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2026
+** corewar
+** File description:
+** insertion
+*/
+
+#include "../../../include/corewar.h"
+
+#ifndef INSERTION_H
+    #define INSERTION_H
+void write_int_to_map(unsigned char *map, int pc, int value);
+#endif
