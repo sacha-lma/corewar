09b96857113fb84db5224a588877f5811be71096
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-15 18:01:12 +0200

    feat: add parse_positive_int function for validating and parsing positive integers in get_cycles

--- src/inputs/get_cycles.c
+++ src/inputs/get_cycles.c
@@ -7,16 +7,33 @@
 
 #include "../../include/corewar.h"
 
+static int parse_positive_int(const char *str, int *value)
+{
+    long long result = 0;
+
+    if (str == NULL || *str == '\0')
+        return 84;
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] < '0' || str[i] > '9')
+            return 84;
+        result = result * 10 + (str[i] - '0');
+        if (result > 2147483647)
+            return 84;
+    }
+    if (result <= 0)
+        return 84;
+    *value = (int)result;
+    return 0;
+}
+
 int get_cycles(stock_main_t *main_struct, char **argv, int pos)
 {
-    char *endptr;
-    long cycles;
+    int cycles;
 
-    cycles = strtol(argv[pos], &endptr, 10);
-    if (*endptr != '\0' || cycles <= 0) {
+    if (parse_positive_int(argv[pos], &cycles) == 84) {
         write(2, "./corewar: -dump: invalid argument\n", 35);
         return 84;
     }
-    main_struct->nb_cycle = (int)cycles;
+    main_struct->nb_cycle = cycles;
     return 0;
 }
