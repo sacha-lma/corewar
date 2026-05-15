ce85faf7dedca884ceedfe2ad96b1854f8bc7a53
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-15 11:09:42 +0200

    refactor: simplify loop function and enhance process handling

--- src/core/loop/loop.c
+++ src/core/loop/loop.c
@@ -7,39 +7,15 @@
 
 #include "../../../include/corewar.h"
 
-const process_call_t process_calls[] = {
-    {op_live},
-    {op_ld},
-    {op_st},
-    {op_add},
-    {op_sub},
-    {op_and},
-    {op_or},
-    {op_xor},
-    {op_zjmp},
-    {op_ldi},
-    {op_sti},
-    {op_fork},
-    {op_lld},
-    {op_lldi},
-    {op_lfork},
-    {op_print},
-    {NULL}
-};
-
-void loop(stock_main_t *main_struct)
+int loop(stock_main_t *main_struct)
 {
-    process_t *current_process = NULL;
-    unsigned char opcode = 0;
-
-    for (node_t *temp = main_struct->processes;
-        temp != NULL;
-        temp = temp->next) {
-        current_process = temp->data;
-        opcode = main_struct->map[current_process->pc % MEM_SIZE];
-        if (opcode >= 1 && opcode <= 16)
-            process_calls[opcode - 1].func(main_struct, current_process);
-        else
-            current_process->pc = (current_process->pc + 1) % MEM_SIZE;
+    if (main_struct->processes == NULL)
+        return 84;
+    call_op(main_struct);
+    main_struct->nb_cycle++;
+    if (main_struct->nb_cycle >= (int)main_struct->next_check_cycle) {
+        check_deaths(main_struct);
+        main_struct->next_check_cycle += main_struct->cycle_to_die;
     }
+    return 0;
 }
