b28d3c421b31e5b3d5ba707c3f2075f777ccff1e
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-15 17:42:55 +0200

    feat: implement op_lldi function for improved argument loading and process state updates

--- src/core/opperations/op_lldi.c
+++ src/core/opperations/op_lldi.c
@@ -7,6 +7,34 @@
 
 #include "../../../include/corewar.h"
 
+static int get_lldi_arg(stock_main_t *main_struct, process_t *process,
+    int type, int offset)
+{
+    int arg = read_arg(main_struct->map, process->pc + offset, type, 1);
+
+    if (type == 0x01)
+        return (arg >= 1 && arg <= REG_NUMBER) ? process->reg[arg - 1] : 0;
+    if (type == 0x03)
+        return read_int_from_map(main_struct->map,
+            (process->pc + arg) % MEM_SIZE);
+    return arg;
+}
+
 void op_lldi(stock_main_t *main_struct, process_t *process)
 {
+    unsigned char coding = main_struct->map[(process->pc + 1) % MEM_SIZE];
+    int type1 = (coding >> 6) & 0x03;
+    int type2 = (coding >> 4) & 0x03;
+    int off2 = 2 + get_arg_size(type1, 1);
+    int off3 = off2 + get_arg_size(type2, 1);
+    int arg1 = get_lldi_arg(main_struct, process, type1, 2);
+    int arg2 = get_lldi_arg(main_struct, process, type2, off2);
+    int reg_dest = main_struct->map[(process->pc + off3) % MEM_SIZE];
+    int addr = (process->pc + arg1 + arg2) % MEM_SIZE;
+    int result = read_int_from_map(main_struct->map, addr);
+
+    if (reg_dest >= 1 && reg_dest <= REG_NUMBER)
+        process->reg[reg_dest - 1] = result;
+    process->carry = (result == 0) ? 1 : 0;
+    process->pc = (process->pc + off3 + 1) % MEM_SIZE;
 }
