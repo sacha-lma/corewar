bb93c1f54fff5fe5a22b78f398ad62cb613a935b
sacha portable <sacha.le-moign-avalos@epitech.eu>
2026-05-15 18:01:23 +0200

    feat: add parse_positive_int function for validating positive integers in get_prog_nb

--- src/inputs/get_prog_nb.c
+++ src/inputs/get_prog_nb.c
@@ -7,16 +7,33 @@
 
 #include "../../include/corewar.h"
 
+static int parse_positive_int(const char *str, int *value)
+{
+    long long result = 0;
+
+    if (str == NULL || *str == '\0')
+        return 84;
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] < '0' || str[i] > '9')
+            return 84;
+        result = result * 10 + (str[i] - '0');
+        if (result > 2147483647)
+            return 84;
+    }
+    if (result <= 0)
+        return 84;
+    *value = (int)result;
+    return 0;
+}
+
 int get_prog_nb(stock_main_t *main_struct, char **argv, int pos)
 {
-    char *endptr;
-    long prog_nb;
+    int prog_nb;
 
-    prog_nb = strtol(argv[pos], &endptr, 10);
-    if (*endptr != '\0' || prog_nb <= 0) {
+    if (parse_positive_int(argv[pos], &prog_nb) == 84) {
         write(2, "./corewar: -n: invalid argument\n", 32);
         return 84;
     }
-    main_struct->champ_info.pending_prog_nb = (int)prog_nb;
+    main_struct->champ_info.pending_prog_nb = prog_nb;
     return 0;
 }
